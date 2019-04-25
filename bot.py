f63a3d65cc5c9e3243c61eb62ac8022bde1fafaa
mocaaoba <49994070+mocaaoba@users.noreply.github.com>
2019-04-25 17:48:16 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,6 +40,7 @@ async def on_message(message):
         newImg = Image.new("RGB", img.size)
         newImg.putdata(newPixels)
 
+        pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
         text = pytesseract.image_to_string(newImg).replace("S", "8").replace("O", "0").replace("Z", "2")
         print(text)
         start = text.find("ID") + 4
