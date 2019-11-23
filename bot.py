192e2187c96d0c052c25b39bb700baeb658455c7
VivereSV <32445070+VivereSV@users.noreply.github.com>
2019-11-23 12:12:15 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ async def on_message(message):
         
         pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
         
-        text = pytesseract.image_to_string(newimg, config='-psm 5').replace("S", "8").replace("O", "0").replace("Z", "2").replace("Q", "0").replace("L", "1").replace("G", "6")
+        text = pytesseract.image_to_string(newimg, config='-psm 7').replace("S", "8").replace("O", "0").replace("Z", "2").replace("Q", "0").replace("L", "1").replace("G", "6")
         saveText = text
         
         # The start of the raid code
